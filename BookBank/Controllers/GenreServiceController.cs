952935e97ef3e19439eaa21258e2903fb7befb02
Kaleem <yousafzaikaleem@gmail.com>
2016-11-10 15:11:31 +0500

    Genre json data is added

---
+++ BookBank/Controllers/GenreServiceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BookBank.Models;
+namespace BookBank.Controllers
+{
+
+    public class GenreServiceController : ApiController
+    {
+        BookStoreEntities db = new BookStoreEntities();
+        public IEnumerable<Genre> Get()
+        {
+           
+                return db.Genres.ToList();
+            
+        }
+        public IEnumerable<Genre> Get(int id)
+        {
+
+            return db.Genres.Where(x=>x.GenreId==id);
+
+        }
+
+    }
+}
